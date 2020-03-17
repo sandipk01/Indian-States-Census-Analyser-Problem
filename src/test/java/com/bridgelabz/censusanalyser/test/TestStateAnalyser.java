75104f63c99b230320458895b8e5dd7908c23fc9
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-17 17:31:50 +0530

    ADDED : given file delimiter is incorrect the should throw no such field exception

--- src/test/java/com/bridgelabz/censusanalyser/test/TestStateAnalyser.java
+++ src/test/java/com/bridgelabz/censusanalyser/test/TestStateAnalyser.java
@@ -17,14 +17,14 @@ public class TestStateAnalyser {
     @Test
     public void GivenNumberOfRecord_WhenCompared_ThenShouldMatch() throws IOException, StateCensusAnalyserException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(CSV_FILE_PATH);
-        Assert.assertEquals(28, stateCensusAnalyser.size());
+        Assert.assertEquals(28, stateCensusAnalyser.load());
     }
 
     @Test
     public void GivenFileName_WhenIncorrect_ThenShouldThrowsNoSuchFileException() throws IOException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(WRONG_CSV_FILE_NAME);
         try {
-            stateCensusAnalyser.size();
+            stateCensusAnalyser.checkCsv();
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.TypeOfException.NO_SUCH_FILE_EXCEPTION, e.type);
         }
@@ -34,9 +34,9 @@ public class TestStateAnalyser {
     public void GivenFileType_WhenInCorrect_ThenShouldThrowNoSuchFileException() throws IOException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(WRONG_CSV_FILE_TYPE);
         try {
-            stateCensusAnalyser.size();
+            stateCensusAnalyser.checkCsv();
         } catch (StateCensusAnalyserException e) {
-            Assert.assertEquals(StateCensusAnalyserException.TypeOfException.NO_SUCH_FILE_EXCEPTION, e.type);
+            Assert.assertEquals(StateCensusAnalyserException.TypeOfException.NO_CSV_FILE, e.type);
         }
     }
 
@@ -44,7 +44,7 @@ public class TestStateAnalyser {
     public void GivenFileDelimiter_WhenInCorrect_ThenShouldThrowNoSuchFieldException() throws IOException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(INCORRECT_CSV_FILE);
         try {
-            stateCensusAnalyser.size();
+            stateCensusAnalyser.checkCsv();
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.TypeOfException.NO_SUCH_FILED_EXCEPTION, e.type);
         }
