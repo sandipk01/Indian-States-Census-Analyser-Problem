16d5c31e3c6abb9dee1dd3de5ef25c1822c7e102
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-17 17:27:17 +0530

    ADDED : no csv file exception type is added

--- src/main/java/com/bridgelabz/censusanalyser/exception/StateCensusAnalyserException.java
+++ src/main/java/com/bridgelabz/censusanalyser/exception/StateCensusAnalyserException.java
@@ -3,7 +3,7 @@ package com.bridgelabz.censusanalyser.exception;
 public class StateCensusAnalyserException extends Exception{
 
     public enum TypeOfException{
-        NO_SUCH_FILE_EXCEPTION
+        NO_SUCH_FILE_EXCEPTION,NO_SUCH_FILED_EXCEPTION,NO_CSV_FILE
     }
 
     public TypeOfException type;
