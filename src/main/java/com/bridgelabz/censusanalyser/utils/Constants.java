0ffe86b3d61a85767d31f892685a8c28265e3054
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-18 16:19:05 +0530

    ADDED : Wrong file name constants name is added

--- src/main/java/com/bridgelabz/censusanalyser/utils/Constants.java
+++ src/main/java/com/bridgelabz/censusanalyser/utils/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String INCORRECT_CSV_FILE = "src/test/resources/statedata/IncorrectStateCensusData.csv";
     public static final String INCORRECT_HEADER_FILE = "src/test/resources/statedata/IncorrectHeaderStateCensusData.csv";
     public static final String STATE_CODE_CSV="src/test/resources/statecode/StateCode.csv";
+    public static final String WRONG_STATE_CODE_FILE_NAME ="src/test/resources/statecode/StateCo.csv";
 }
