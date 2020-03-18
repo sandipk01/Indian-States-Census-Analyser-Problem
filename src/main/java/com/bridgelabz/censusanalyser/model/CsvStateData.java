066997226e8267f0a536f8fececd711dc7fc5fc6
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-18 14:14:31 +0530

    REFACTORED : Method name corrected

---
+++ src/main/java/com/bridgelabz/censusanalyser/model/CsvStateData.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.censusanalyser.model;
+
+public class CsvStateModel {
+}
--- src/test/java/com/bridgelabz/censusanalyser/test/TestStateAnalyser.java
+++ src/test/java/com/bridgelabz/censusanalyser/test/TestStateAnalyser.java
@@ -9,20 +9,20 @@ import java.io.IOException;
 
 public class TestStateAnalyser {
 
-    private static final String CSV_FILE_PATH = "/home/sandip/Documents/Indian-States-Census-Analyser-Problem/src/test/resources/StateCensusData.csv";
-    private static final String WRONG_CSV_FILE_NAME = "/home/sandip/Documents/Indian-States-Census-Analyser-Problem/src/test/resources/StateCensus.csv";
-    private static final String WRONG_CSV_FILE_TYPE = "/home/sandip/Documents/Indian-States-Census-Analyser-Problem/src/test/resources/StateCensusData.txt";
-    private static final String INCORRECT_CSV_FILE = "/home/sandip/Documents/Indian-States-Census-Analyser-Problem/src/test/resources/IncorrectStateCensusData.csv";
-    private static final String INCORRECT_HEADER_FILE = "/home/sandip/Documents/Indian-States-Census-Analyser-Problem/src/test/resources/IncorrectHeaderStateCensusData.csv";
+    private static final String CSV_FILE_PATH = "/home/admin1/IdeaProjects/Indian-States-Census-Analyser-Problem/src/test/resources/StateCensusData.csv";
+    private static final String WRONG_CSV_FILE_NAME = "/home/admin1/IdeaProjects/Indian-States-Census-Analyser-Problem/src/test/resources/StateCensus.csv";
+    private static final String WRONG_CSV_FILE_TYPE = "/home/admin1/IdeaProjects/Indian-States-Census-Analyser-Problem/src/test/resources/StateCensusData.txt";
+    private static final String INCORRECT_CSV_FILE = "/home/admin1/IdeaProjects/Indian-States-Census-Analyser-Problem/src/test/resources/IncorrectStateCensusData.csv";
+    private static final String INCORRECT_HEADER_FILE = "/home/admin1/IdeaProjects/Indian-States-Census-Analyser-Problem/src/test/resources/IncorrectHeaderStateCensusData.csv";
 
     @Test
-    public void GivenNumberOfRecord_WhenCompared_ThenShouldMatch() throws IOException, StateCensusAnalyserException {
+    public void givenNumberOfRecord_WhenCompared_ThenShouldMatch() throws IOException, StateCensusAnalyserException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(CSV_FILE_PATH);
         Assert.assertEquals(28, stateCensusAnalyser.load());
     }
 
     @Test
-    public void GivenFileName_WhenIncorrect_ThenShouldThrowsNoSuchFileException() throws IOException {
+    public void givenFileName_WhenIncorrect_ThenShouldThrowsNoSuchFileException() throws IOException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(WRONG_CSV_FILE_NAME);
         try {
             stateCensusAnalyser.checkCsv();
@@ -32,7 +32,7 @@ public class TestStateAnalyser {
     }
 
     @Test
-    public void GivenFileType_WhenInCorrect_ThenShouldThrowNoSuchFileException() throws IOException {
+    public void givenFileType_WhenInCorrect_ThenShouldThrowNoSuchFileException() throws IOException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(WRONG_CSV_FILE_TYPE);
         try {
             stateCensusAnalyser.checkCsv();
@@ -42,7 +42,7 @@ public class TestStateAnalyser {
     }
 
     @Test
-    public void GivenFileDelimiter_WhenInCorrect_ThenShouldThrowIncorrectDelimiterOrHeaderException() throws IOException {
+    public void givenFileDelimiter_WhenInCorrect_ThenShouldThrowIncorrectDelimiterOrHeaderException() throws IOException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(INCORRECT_CSV_FILE);
         try {
             stateCensusAnalyser.checkCsv();
@@ -52,7 +52,7 @@ public class TestStateAnalyser {
     }
 
     @Test
-    public void GivenFileHeader_WhenInCorrect_ThenShouldThrowIncorrectDelimiterOrHeaderException() throws IOException {
+    public void givenFileHeader_WhenInCorrect_ThenShouldThrowIncorrectDelimiterOrHeaderException() throws IOException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(INCORRECT_HEADER_FILE);
         try {
             stateCensusAnalyser.checkCsv();
