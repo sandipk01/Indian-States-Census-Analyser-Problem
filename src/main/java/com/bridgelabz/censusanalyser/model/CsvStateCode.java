b19fa5529798c108c99610dcf9c776f38959ac09
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-18 16:34:09 +0530

    REFACTORED : Required true attribute is added

--- src/main/java/com/bridgelabz/censusanalyser/model/CsvStateCode.java
+++ src/main/java/com/bridgelabz/censusanalyser/model/CsvStateCode.java
@@ -3,12 +3,12 @@ package com.bridgelabz.censusanalyser.model;
 import com.opencsv.bean.CsvBindByName;
 
 public class CsvStateCode {
-    @CsvBindByName(column = "SrNo")
+    @CsvBindByName(column = "SrNo", required = true)
     private int srNo;
-    @CsvBindByName(column = "StateName")
+    @CsvBindByName(column = "StateName", required = true)
     private String stateName;
-    @CsvBindByName(column = "TIN")
+    @CsvBindByName(column = "TIN", required = true)
     private int tin;
-    @CsvBindByName(column = "StateCode")
+    @CsvBindByName(column = "StateCode", required = true)
     private String stateCode;
 }
