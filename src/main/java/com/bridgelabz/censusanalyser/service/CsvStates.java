c721024af44a2017269062a83eee3bdf48829e75
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-18 16:17:54 +0530

    REFACTORE : throw no such file exception is added

--- src/main/java/com/bridgelabz/censusanalyser/service/CsvStates.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/CsvStates.java
@@ -1,14 +1,14 @@
 package com.bridgelabz.censusanalyser.service;
 
 import com.bridgelabz.censusanalyser.exception.StateCensusAnalyserException;
-import com.bridgelabz.censusanalyser.model.CSVStateCensus;
 import com.bridgelabz.censusanalyser.model.CsvStateCode;
+import com.bridgelabz.censusanalyser.utils.Utils;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
@@ -22,17 +22,22 @@ public class CsvStates {
         this.filename = filename;
     }
 
-    public int loadCsvState() throws IOException {
+    public int loadCsvState() throws IOException, StateCensusAnalyserException {
         int counter = 0;
-        Reader reader = Files.newBufferedReader(Paths.get(filename));
-        CsvToBean<CsvStateCode> csvToBean = new CsvToBeanBuilder(reader)
-                .withType(CsvStateCode.class)
-                .withIgnoreLeadingWhiteSpace(true)
-                .build();
-        Iterator iterator = csvToBean.iterator();
-        while (iterator.hasNext()) {
-            iterator.next();
-            counter++;
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(filename));
+
+            CsvToBean<CsvStateCode> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CsvStateCode.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator iterator = csvToBean.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                counter++;
+            }
+        } catch (NoSuchFileException e) {
+            throw new StateCensusAnalyserException("No such file found", StateCensusAnalyserException.TypeOfException.NO_SUCH_FILE_EXCEPTION);
         }
         return counter;
     }
