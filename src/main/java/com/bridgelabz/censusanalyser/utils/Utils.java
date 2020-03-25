2537af6b9f45cf36abdf3669b1bace0222bc8954
sandipk01 <sandipkengar01@gmail.com>
2020-03-25 08:49:09 +0530

    REMOVED  : Unused Imports

--- src/main/java/com/bridgelabz/censusanalyser/utils/Utils.java
+++ src/main/java/com/bridgelabz/censusanalyser/utils/Utils.java
@@ -1,14 +1,14 @@
 package com.bridgelabz.censusanalyser.utils;
 
-import javax.swing.*;
 import java.io.File;
+
 
 public class Utils {
 
     public static String getFileExtension(File file) {
         String fileName = file.getName();
-        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
-            return fileName.substring(fileName.lastIndexOf(".")+1);
+        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
         else return "";
     }
 }
