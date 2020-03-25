09bba3220a4992903a96f999eabcf4ccd0ec4f9c
sandipk01 <sandipkengar01@gmail.com>
2020-03-25 09:37:13 +0530

    ADDED : Comments are added

--- src/main/java/com/bridgelabz/censusanalyser/utils/Utils.java
+++ src/main/java/com/bridgelabz/censusanalyser/utils/Utils.java
@@ -2,9 +2,9 @@ package com.bridgelabz.censusanalyser.utils;
 
 import java.io.File;
 
-
 public class Utils {
 
+    //Method to return file extension.
     public static String getFileExtension(File file) {
         String fileName = file.getName();
         if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
