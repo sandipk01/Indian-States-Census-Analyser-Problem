3d6796741910e25f081654b1ee85e69e88b327ce
sandipk01 <sandipkengar01@gmail.com>
2020-03-26 06:28:26 +0530

    ADDED : Method for getting sorted data in json format

--- src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.censusanalyser.service;
 
 import com.bridgelabz.censusanalyser.exception.CSVBuilderException;
+import com.bridgelabz.censusanalyser.model.CSVStateCensus;
 import com.bridgelabz.censusanalyser.utils.Utils;
+import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,19 +11,21 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
 
-public class StateCensusAnalyser {
+public class StateCensusAnalyser<E> {
 
     private String fileName;
-    private Class className;
+    private final Class<E> className;
 
-    public StateCensusAnalyser(String fileName, Class className) {
+    public StateCensusAnalyser(String fileName, Class<E> className) {
         this.fileName = fileName;
         this.className = className;
     }
 
     //Checking whether file is csv or not.
-    public int checkCsv() throws IOException, CSVBuilderException {
+    public List<E> checkCsv() throws IOException, CSVBuilderException {
         if (Utils.getFileExtension(new File(fileName)).equals("csv"))
             return load();
         else
@@ -30,17 +34,22 @@ public class StateCensusAnalyser {
     }
 
     //Loading csv file data
-    public int load() throws IOException, CSVBuilderException {
-        int counter = 0;
+    public List<E> load() throws IOException, CSVBuilderException {
+        List<E> userList = null;
         try (Reader reader = Files.newBufferedReader(Paths.get(fileName))) {
-            ICSVBuilder csvBuilder=CSVBuilderFactory.getInstance();
-            counter=csvBuilder.loadList(reader,className).size();
+            ICSVBuilder csvBuilder = CSVBuilderFactory.getInstance();
+            userList = csvBuilder.loadList(reader, className);
         } catch (NoSuchFileException e) {
             throw new CSVBuilderException("No such file found", CSVBuilderException.TypeOfException.NO_SUCH_FILE_EXCEPTION);
         } catch (RuntimeException e) {
             throw new CSVBuilderException("No such field found", CSVBuilderException.TypeOfException.INCORRECT_DELIMITER_OR_HEADER);
         }
-        return counter;
+        return userList;
     }
 
+    //Getting sorted Json data
+    public String getSortedJsonData(Comparator<E> comparator,List<E> list){
+        ICSVBuilder csvBuilder = CSVBuilderFactory.getInstance();
+        return csvBuilder.listConvertingToJson(csvBuilder.sortingList(comparator,list));
+    }
 }
