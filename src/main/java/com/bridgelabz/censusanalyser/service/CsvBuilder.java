d190b9feeb7eea5757e3e0622cfc868a1ddb717e
sandipk01 <sandipkengar01@gmail.com>
2020-03-26 06:24:47 +0530

    ADDED : Method to sortList and listConvertingToJson added

--- src/main/java/com/bridgelabz/censusanalyser/service/CsvBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/CsvBuilder.java
@@ -1,10 +1,12 @@
 package com.bridgelabz.censusanalyser.service;
 
 import com.bridgelabz.censusanalyser.model.CSVStateCensus;
+import com.google.gson.Gson;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,4 +33,25 @@ public class CsvBuilder implements ICSVBuilder {
         }
         return counter;
     }
+
+    //Method for sorting using comparator
+    public <E> List<E> sortingList(Comparator<E> comparator, List censusRecords) {
+        for (int iterate = 0; iterate < censusRecords.size() - 1; iterate++) {
+            for (int Inneriterate = 0; Inneriterate < censusRecords.size() - iterate - 1; Inneriterate++) {
+                E census1 = (E) censusRecords.get(Inneriterate);
+                E census2 = (E) censusRecords.get(Inneriterate + 1);
+                if (comparator.compare(census1, census2) > 0) {
+                    censusRecords.set(Inneriterate, census2);
+                    censusRecords.set(Inneriterate + 1, census1);
+                }
+            }
+        }
+        return  censusRecords;
+    }
+
+    //Method to convert data in json format
+    public <E> String listConvertingToJson(List<E> list) {
+         return new Gson().toJson(list);
+    }
+
 }
