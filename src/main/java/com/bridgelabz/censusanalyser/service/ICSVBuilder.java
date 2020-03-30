2c8add8f3687ad1099108bc62388cefed79ca06d
sandipk01 <sandipkengar01@gmail.com>
2020-03-30 11:12:58 +0530

    REFACTORED : Changed method declareation

--- src/main/java/com/bridgelabz/censusanalyser/service/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/ICSVBuilder.java
@@ -2,18 +2,13 @@ package com.bridgelabz.censusanalyser.service;
 
 import java.io.Reader;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 public interface ICSVBuilder {
 
-    public <E> Iterator<E> load(Reader reader, Class className);
+    public <S,T> HashMap<S,T> loadHashMap(Reader reader, Class className);
 
-    public <E> List loadList(Reader reader, Class csvClass);
-
-    public <E> int size(Iterator<E> iterator);
-
-    public <E> List<E> sortingList(Comparator<E> comparator, List censusRecords);
-
-    public <E> String listConvertingToJson(List<E> list);
+    public <S,T> HashMap<S,T> sortingList(Comparator comparator, HashMap<S,T> censusHashMap);
 }
