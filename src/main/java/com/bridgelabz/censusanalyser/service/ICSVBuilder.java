0afdff816efd9a719d34526e242be29a97a89af3
sandipk01 <sandipkengar01@gmail.com>
2020-03-31 13:27:21 +0530

    REFCATORED : Methods declaration refactored

--- src/main/java/com/bridgelabz/censusanalyser/service/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/ICSVBuilder.java
@@ -1,14 +1,13 @@
 package com.bridgelabz.censusanalyser.service;
 
 import java.io.Reader;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public interface ICSVBuilder {
 
-    public <S,T> HashMap<S,T> loadHashMap(Reader reader, Class className);
+    public <E> List<E> loadFromList(Reader reader, Class className);
 
-    public <S,T> HashMap<S,T> sortingList(Comparator comparator, HashMap<S,T> censusHashMap);
+    public <S, T> LinkedHashMap<S, T> sorting(Comparator<Map.Entry<S, T>> comparator, HashMap<S, T> censusHashMap);
+
+    public <E> Iterator<E> loadFromIterator(Reader reader, Class className);
 }
