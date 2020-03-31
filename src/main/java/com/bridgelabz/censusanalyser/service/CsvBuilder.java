caa4d08cbf135e06fb10ddc0a14793e4b61a67e8
sandipk01 <sandipkengar01@gmail.com>
2020-03-31 13:26:18 +0530

    REFACTORED : Data loading using iterator

--- src/main/java/com/bridgelabz/censusanalyser/service/CsvBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/CsvBuilder.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.censusanalyser.service;
 
-import com.google.gson.Gson;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
@@ -10,22 +9,19 @@ import java.util.*;
 public class CsvBuilder implements ICSVBuilder {
 
     //Generic method to load the csv data using iterator
-    public <E> Iterator<E> load(Reader reader, Class className) {
-        CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(reader).withIgnoreLeadingWhiteSpace(true).withType(className).build();
+    public <E> Iterator<E> loadFromIterator(Reader reader, Class className) {
+        CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(reader)
+                .withIgnoreLeadingWhiteSpace(true)
+                .withType(className).build();
         return csvToBean.iterator();
     }
 
     //Generic method to load csv data using list
-    public <S,T> HashMap<S,T> loadHashMap(Reader reader, Class className) {
-        CsvToBean csvToBean = new CsvToBeanBuilder(reader).withIgnoreLeadingWhiteSpace(true).withType(className).build();
-        List list=csvToBean.parse();
-        HashMap<Integer ,Object> map = new HashMap<>();
-        Integer count = 0;
-        for (Object record:list) {
-            map.put(count,record);
-            count++;
-        }
-        return (HashMap<S, T>) map;
+    public <E> List<E> loadFromList(Reader reader, Class className) {
+        CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(reader)
+                .withIgnoreLeadingWhiteSpace(true)
+                .withType(className).build();
+        return csvToBean.parse();
     }
 
     //Method for get size of data
@@ -38,12 +34,12 @@ public class CsvBuilder implements ICSVBuilder {
         return counter;
     }
 
-    //Method for sorting using comparator
-    public <S,T> HashMap<S,T> sortingList(Comparator comparator, HashMap<S,T> censusHashMap) {
+    //Method for sorting csv data
+    public <S, T> LinkedHashMap<S, T> sorting(Comparator<Map.Entry<S, T>> comparator, HashMap<S, T> censusHashMap) {
         Set<Map.Entry<S, T>> entries = censusHashMap.entrySet();
         List<Map.Entry<S, T>> listOfEntries = new ArrayList<>(entries);
         Collections.sort(listOfEntries, comparator);
-        LinkedHashMap<S, T> sortedByValue = new LinkedHashMap<S,T>(listOfEntries.size());
+        LinkedHashMap<S, T> sortedByValue = new LinkedHashMap<S, T>(listOfEntries.size());
         // copying entries from List to Map
         for (Map.Entry<S, T> entry : listOfEntries) {
             sortedByValue.put(entry.getKey(), entry.getValue());
