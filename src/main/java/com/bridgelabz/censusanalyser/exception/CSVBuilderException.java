61e83ebfc55597f8133a337db1b4a01b83761ff3
sandipk01 <sandipkengar01@gmail.com>
2020-04-01 06:17:14 +0530

    ADDED : NO_CENSUS_DATA,NO_CENSUS_CODE_DATA exceptions are added

--- src/main/java/com/bridgelabz/censusanalyser/exception/CSVBuilderException.java
+++ src/main/java/com/bridgelabz/censusanalyser/exception/CSVBuilderException.java
@@ -3,7 +3,7 @@ package com.bridgelabz.censusanalyser.exception;
 public class CSVBuilderException extends Exception{
 
     public enum TypeOfException{
-        NO_SUCH_FILE_EXCEPTION,INCORRECT_DELIMITER_OR_HEADER,NO_CSV_FILE
+        NO_SUCH_FILE_EXCEPTION,INCORRECT_DELIMITER_OR_HEADER,NO_CSV_FILE,NO_CENSUS_DATA,NO_CENSUS_CODE_DATA;
     }
 
     public TypeOfException type;
