5746995a97cf1f8b8b51293eb0114a6cc48a340e
sandipk01 <sandipkengar01@gmail.com>
2020-04-01 15:58:20 +0530

    ADDED : Method for sorting according to area

--- src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
@@ -114,4 +114,17 @@ public class StateCensusAnalyser<E> {
         String sortedStateCensusJson = new Gson().toJson(list);
         return sortedStateCensusJson;
     }
+
+    //Method for sorting data on basis of population density
+    public String getStateWiseSortedStateArea(HashMap<String, IndianCensusDao> hashMap) throws CSVBuilderException {
+        if (hashMap == null || hashMap.size() == 0)
+            throw new CSVBuilderException("No DensityPerSquareKM State Data", CSVBuilderException.TypeOfException.NO_CENSUS_DATA);
+        ICSVBuilder csvBuilder = CSVBuilderFactory.getInstance();
+        Comparator<Map.Entry<String, IndianCensusDao>> censusComparator = Comparator.comparing(censusDAOEntry -> censusDAOEntry.getValue().areaInSqKm);
+        LinkedHashMap<String, IndianCensusDao> sortedByValue = csvBuilder.sorting(censusComparator,hashMap);
+        ArrayList<IndianCensusDao> list = new ArrayList<>(sortedByValue.values());
+        Collections.reverse(list);
+        String sortedStateCensusJson = new Gson().toJson(list);
+        return sortedStateCensusJson;
+    }
 }
