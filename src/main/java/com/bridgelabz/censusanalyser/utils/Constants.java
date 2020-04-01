bd9f067033bc6ddc2522d1ac566b2de29289d8e4
sandipk01 <sandipkengar01@gmail.com>
2020-04-01 06:20:26 +0530

    ADDED : Empty census file and Empty state code file path added

--- src/main/java/com/bridgelabz/censusanalyser/utils/Constants.java
+++ src/main/java/com/bridgelabz/censusanalyser/utils/Constants.java
@@ -6,9 +6,11 @@ public class Constants {
     public static final String WRONG_CSV_FILE_TYPE = "src/test/resources/statedata/StateCensusData.txt";
     public static final String INCORRECT_CSV_FILE = "src/test/resources/statedata/IncorrectStateCensusData.csv";
     public static final String INCORRECT_HEADER_FILE = "src/test/resources/statedata/IncorrectHeaderStateCensusData.csv";
+    public static final String EMPTY_STATE_CENSUS_FILE="src/test/resources/statedata/EmptyStateCensusData.csv";
     public static final String STATE_CODE_CSV = "src/test/resources/statecode/StateCode.csv";
     public static final String WRONG_STATE_CODE_FILE_NAME = "src/test/resources/statecode/StateCo.csv";
     public static final String WRONG_STATE_CODE_FILE_TYPE = "src/test/resources/statecode/StateCode.txt";
     public static final String INCORRECT_STATE_CODE_CSV_FILE = "src/test/resources/statecode/IncorrectDelimiterStateCode.csv";
     public static final String INCORRECT_STATE_CODE_HEADER_FILE = "src/test/resources/statecode/IncorrectHeaderStateCode.csv";
+    public static final String EMPTY_STATE_CODE_FILE="src/test/resources/statecode/EmptyStateCodeData.csv";
 }
