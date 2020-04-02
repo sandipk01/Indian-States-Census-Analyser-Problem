cd1e28f57be68898c3feb773853b4dd5ba0e0aa5
sandipk01 <sandipkengar01@gmail.com>
2020-04-02 07:23:33 +0530

    REFACTORE : Refactored areaInSqKm and densityPerSqKm integer to double

--- src/main/java/com/bridgelabz/censusanalyser/dao/CensusDao.java
+++ src/main/java/com/bridgelabz/censusanalyser/dao/CensusDao.java
@@ -2,15 +2,16 @@ package com.bridgelabz.censusanalyser.dao;
 
 import com.bridgelabz.censusanalyser.model.CSVStateCensus;
 import com.bridgelabz.censusanalyser.model.CsvStateCode;
+import com.bridgelabz.censusanalyser.model.UsCensusData;
 
 public class CensusDao {
     public String state;
-    public int areaInSqKm;
-    public int densityPerSqKm;
-    public int population;
+    public Double areaInSqKm;
+    public Double densityPerSqKm;
+    public Integer population;
     public String stateCode;
-    public int srNo;
-    public int tin;
+    public Integer srNo;
+    public Integer tin;
 
     public CensusDao(CSVStateCensus csvStateCensus) {
         state = csvStateCensus.getStateName();
@@ -24,5 +25,13 @@ public class CensusDao {
         srNo = csvStateCode.getSrNo();
         state = csvStateCode.getStateName();
         tin = csvStateCode.getTin();
+    }
+
+    public CensusDao(UsCensusData usCensusData){
+        stateCode=usCensusData.getStateId();
+        areaInSqKm=usCensusData.getArea();
+        state=usCensusData.getState();
+        densityPerSqKm=usCensusData.getPopulationDensity();
+        population=usCensusData.getPopulation();
     }
 }
