804194aafa4e2372ddd614a700a02bd4c17b97b0
sandipk01 <sandipkengar01@gmail.com>
2020-04-04 10:41:40 +0530

    ADDED : Loading us data

--- src/main/java/com/bridgelabz/censusanalyser/adapter/USCensusAdapter.java
+++ src/main/java/com/bridgelabz/censusanalyser/adapter/USCensusAdapter.java
@@ -1,4 +1,17 @@
 package com.bridgelabz.censusanalyser.adapter;
 
-public class USCensusAdapter {
+import com.bridgelabz.censusanalyser.dao.CensusDao;
+import com.bridgelabz.censusanalyser.exception.CSVBuilderException;
+import com.bridgelabz.censusanalyser.model.UsCensusData;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class USCensusAdapter extends CensusAdapter {
+
+    //Method for loading us census data
+    @Override
+    public HashMap<String, CensusDao> loadingCensusData(String... csvFilePath) throws IOException, CSVBuilderException {
+        return loadingCensusData(UsCensusData.class, csvFilePath[0]);
+    }
 }
