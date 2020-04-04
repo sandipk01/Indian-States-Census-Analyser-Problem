d2b8a487542a27f75ec476ca05d560fb2245f855
sandipk01 <sandipkengar01@gmail.com>
2020-04-04 10:39:26 +0530

    ADDED : Factory method to choose load india or us data

--- src/main/java/com/bridgelabz/censusanalyser/adapter/CensusAdapterFactory.java
+++ src/main/java/com/bridgelabz/censusanalyser/adapter/CensusAdapterFactory.java
@@ -1,4 +1,15 @@
 package com.bridgelabz.censusanalyser.adapter;
 
+import com.bridgelabz.censusanalyser.service.StateCensusAnalyser;
+
 public class CensusAdapterFactory {
+
+    //Factory method to get india or us data
+    public static CensusAdapter getCensusData(StateCensusAnalyser.COUNTRY country) {
+        if (country.equals(StateCensusAnalyser.COUNTRY.INDIA))
+            return new IndiaCensusAdapter();
+        if (country.equals(StateCensusAnalyser.COUNTRY.US))
+            return new USCensusAdapter();
+        return null;
+    }
 }
