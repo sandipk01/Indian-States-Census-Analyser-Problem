491aa92d31a0c210420352c672d3ce0e7cf387cf
sandipk01 <sandipkengar01@gmail.com>
2020-04-07 09:49:17 +0530

    ADDED : Method to sort data according to population and density

--- src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
@@ -1,21 +1,21 @@
 package com.bridgelabz.censusanalyser.service;
 
-import com.bridgelabz.censusanalyser.adapter.CensusAdapter;
-import com.bridgelabz.censusanalyser.adapter.CensusAdapterFactory;
-import com.bridgelabz.censusanalyser.dao.CensusDao;
-import com.bridgelabz.censusanalyser.exception.CSVBuilderException;
-import com.bridgelabz.censusanalyser.model.CSVStateCensus;
-import com.bridgelabz.censusanalyser.model.CsvStateCode;
-import com.bridgelabz.censusanalyser.model.UsCensusData;
-import com.bridgelabz.censusanalyser.utils.Utils;
-import com.google.gson.Gson;
+        import com.bridgelabz.censusanalyser.adapter.CensusAdapter;
+        import com.bridgelabz.censusanalyser.adapter.CensusAdapterFactory;
+        import com.bridgelabz.censusanalyser.dao.CensusDao;
+        import com.bridgelabz.censusanalyser.exception.CSVBuilderException;
+        import com.bridgelabz.censusanalyser.model.CSVStateCensus;
+        import com.bridgelabz.censusanalyser.model.CsvStateCode;
+        import com.bridgelabz.censusanalyser.model.UsCensusData;
+        import com.bridgelabz.censusanalyser.utils.Utils;
+        import com.google.gson.Gson;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.CountDownLatch;
-import java.util.stream.Collectors;
+        import java.io.File;
+        import java.io.IOException;
+        import java.util.*;
+        import java.util.concurrent.CountDownLatch;
+        import java.util.stream.Collectors;
 
 public class StateCensusAnalyser {
 
@@ -52,6 +52,17 @@ public class StateCensusAnalyser {
         ArrayList censusDTO = censusDAOMap.values().stream()
                 .sorted(CensusDao.getSortComparator(mode))
                 .map(censusDAO -> censusDAO.getCensusDTO(country))
+                .collect(Collectors.toCollection(ArrayList::new));
+        return new Gson().toJson(censusDTO);
+    }
+
+    //Method to sort data according to population and density.
+    public String getSortByPopulationAndDensity(HashMap<String,CensusDao> censusDAOMap) throws CSVBuilderException {
+        if (censusDAOMap == null || censusDAOMap.size() == 0)
+            throw new CSVBuilderException("No Census Data", CSVBuilderException.TypeOfException.NO_CENSUS_DATA);
+        ArrayList censusDTO = censusDAOMap.values().stream()
+                .sorted(Comparator.comparingDouble(CensusDao::getPopulation).thenComparingDouble(CensusDao::getPopulationDensity).reversed())
+                .map(c -> c.getCensusDTO(country))
                 .collect(Collectors.toCollection(ArrayList::new));
         return new Gson().toJson(censusDTO);
     }
